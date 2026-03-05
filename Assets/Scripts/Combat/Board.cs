b128882945dc4d66f409b7c8210f625050524cd5
Lingly70 <manshenhehe@gmail.com>
2026-03-05 17:32:23 -0500

    Remove redundant Board.cs

--- Assets/Scripts/Combat/Board.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-//TODO: complete all helper functions for validating commands.
-public class Board : MonoBehaviour
-{
-    public bool WithinBounds(Tile tile) => true;
-    public bool CanMove(Tile from, Tile to, int movementAvailable) => true;
-}
