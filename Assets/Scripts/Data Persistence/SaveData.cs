117d9356649d339b1eb12eb0049a08f414b660d9
mmiladinovgenov <mmiladinovgenov@luc.edu>
2026-03-21 22:25:00 -0400

    Added id field to save data as bare-bones json compatibility

--- Assets/Scripts/Data Persistence/SaveData.cs
+++ Assets/Scripts/Data Persistence/SaveData.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class SaveData {
     [SerializeField]
     private string name; //todo
+    private int id;
     //Constructor
-    public SaveData(string name) {
+    public SaveData(string name, int id) {
         this.name = name;
+        this.id = id;
     }
 
     public string getName(){
