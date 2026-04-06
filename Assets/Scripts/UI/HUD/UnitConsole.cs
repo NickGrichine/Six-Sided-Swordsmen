e29a6307ffbccf9bd0023d6ca68805f79e1918b9
Kinson <95203053+kinsonchk@users.noreply.github.com>
2026-04-05 21:33:19 -0400

    Revert "Changes to interaction with console"
    This reverts commit 6dc84f343e645291faa622e90a366df0a0dd8b7a.

--- Assets/Scripts/UI/HUD/UnitConsole.cs
+++ Assets/Scripts/UI/HUD/UnitConsole.cs
@@ -10,8 +10,6 @@ public class UnitConsole : Singleton<UnitConsole>
 {
     private const int _command_size = 5;
 
-    public event Action<UnitCommandSO> onCommandSelected;
-
     public CustomButton[] commandButtons = new CustomButton[_command_size];
     [SerializeField] private CustomButton unitIcon;
 
@@ -32,12 +30,6 @@ public class UnitConsole : Singleton<UnitConsole>
         ClearUnitConsole();
         if (GridEventHandler.Instance)
             GridEventHandler.Instance.onTileClicked += UpdateUnitConsole;
-    }
-
-    private void OnDestroy()
-    {
-        if (GridEventHandler.Instance)
-            GridEventHandler.Instance.onTileClicked -= UpdateUnitConsole;
     }
 
     public void UpdateUnitConsole(Tile tile)
@@ -81,7 +73,8 @@ public class UnitConsole : Singleton<UnitConsole>
         // Set command buttons:
         foreach (UnitCommandSO command in unitController.commands)
         {
-            SetCommandButton(OnCommandClicked, null, command);
+            // TODO: add onClick/onHover Actions:
+            SetCommandButton(null, null, command);
         }
 
         // TODO: Set unit icon:
@@ -142,17 +135,6 @@ public class UnitConsole : Singleton<UnitConsole>
             command.ClearIcon();
             command.SetState(Button.BUTTON_STATE.INACTIVE);
         }
-    }
-
-    private void OnCommandClicked(Button button)
-    {
-        if (!(button is CustomButton customButton))
-            return;
-
-        if (!(customButton.displayedObject is UnitCommandSO unitCommand))
-            return;
-
-        onCommandSelected?.Invoke(unitCommand);
     }
 
     /// ------------------
